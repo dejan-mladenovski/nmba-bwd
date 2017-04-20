c8edb4767970f2bb446ee82973138c7add0f2d5d
Dejan Mladenovski <Dejan.Mladenovski@netcetera.com>
2017-04-20 19:38:53 +0200

    Form validation example in About component

--- nmba-contacts-list/src/app/about/about.component.ts
+++ nmba-contacts-list/src/app/about/about.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  contact = {
+    name: "",
+    surname: "Mladenovski"
+  };
   constructor() { }
 
   ngOnInit() {
